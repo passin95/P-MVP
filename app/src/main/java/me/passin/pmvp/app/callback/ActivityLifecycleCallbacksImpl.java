c59b58be2eb4603179208a558d8e5f6f40a46a71
passin <zengbinbinpassin@foxmail.com>
2018-08-19 20:29:47 +0800

    modify activity lifecycle log print level to D

--- app/src/main/java/me/passin/pmvp/app/callback/ActivityLifecycleCallbacksImpl.java
+++ app/src/main/java/me/passin/pmvp/app/callback/ActivityLifecycleCallbacksImpl.java
@@ -23,12 +23,12 @@ public class ActivityLifecycleCallbacksImpl implements Application.ActivityLifec
     @Override
     public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
         ScreenUtils.adaptScreen4VerticalSlide(activity,360);
-        Timber.w(activity + " - onActivityCreated");
+        Timber.d(activity + " - onActivityCreated");
     }
 
     @Override
     public void onActivityStarted(Activity activity) {
-        Timber.w(activity + " - onActivityStarted");
+        Timber.d(activity + " - onActivityStarted");
         if (!activity.getIntent().getBooleanExtra("isInitToolbar", false)) {
             //由于加强框架的兼容性,故将 setContentView 放到 onActivityCreated 之后,onActivityStarted 之前执行
             //而 findViewById 必须在 Activity setContentView() 后才有效,所以将以下代码从之前的 onActivityCreated 中移动到 onActivityStarted 中执行
@@ -58,27 +58,27 @@ public class ActivityLifecycleCallbacksImpl implements Application.ActivityLifec
 
     @Override
     public void onActivityResumed(Activity activity) {
-        Timber.w(activity + " - onActivityResumed");
+        Timber.d(activity + " - onActivityResumed");
     }
 
     @Override
     public void onActivityPaused(Activity activity) {
-        Timber.w(activity + " - onActivityPaused");
+        Timber.d(activity + " - onActivityPaused");
     }
 
     @Override
     public void onActivityStopped(Activity activity) {
-        Timber.w(activity + " - onActivityStopped");
+        Timber.d(activity + " - onActivityStopped");
     }
 
     @Override
     public void onActivitySaveInstanceState(Activity activity, Bundle outState) {
-        Timber.w(activity + " - onActivitySaveInstanceState");
+        Timber.d(activity + " - onActivitySaveInstanceState");
     }
 
     @Override
     public void onActivityDestroyed(Activity activity) {
-        Timber.w(activity + " - onActivityDestroyed");
+        Timber.d(activity + " - onActivityDestroyed");
         //横竖屏切换或配置改变时, Activity 会被重新创建实例, 但 Bundle 中的基础数据会被保存下来,移除该数据是为了保证重新创建的实例可以正常工作
         activity.getIntent().removeExtra("isInitToolbar");
     }
