0f15bb6fa354d2b29c947f9fea9cac8f5ac81c2e
passin <zengbinbinpassin@foxmail.com>
2018-08-20 18:05:09 +0800

    accelerate APP start speed through improve Retrofit init way

--- pmvp/src/main/java/com/passin/pmvp/di/module/AppModule.java
+++ pmvp/src/main/java/com/passin/pmvp/di/module/AppModule.java
@@ -3,24 +3,20 @@ package com.passin.pmvp.di.module;
 import android.app.Application;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.util.ArrayMap;
-
+import com.passin.pmvp.base.delegate.ActivityLifecycle;
+import com.passin.pmvp.base.delegate.FragmentLifecycle;
 import com.passin.pmvp.http.repository.IRepositoryManager;
 import com.passin.pmvp.http.repository.RepositoryManager;
 import com.passin.pmvp.integration.AppManager;
 import com.passin.pmvp.integration.cache.Cache;
 import com.passin.pmvp.integration.cache.CacheType;
-import com.passin.pmvp.base.delegate.ActivityLifecycle;
-import com.passin.pmvp.base.delegate.FragmentLifecycle;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import javax.inject.Singleton;
-
 import dagger.Binds;
 import dagger.Module;
 import dagger.Provides;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import javax.inject.Singleton;
 
 /**
  * <pre>
@@ -32,13 +28,10 @@ import dagger.Provides;
 @Module
 public abstract class AppModule {
 
-    @Binds
-    abstract IRepositoryManager bindRepositoryManager(RepositoryManager repositoryManager);
-
     @Singleton
     @Provides
     static Cache<String, Object> provideLruExtras(Cache.Factory cacheFactory) {
-        return cacheFactory.build(CacheType.EXTRAS);
+        return cacheFactory.build(CacheType.LRU_EXTRAS);
     }
 
     @Singleton
@@ -53,17 +46,22 @@ public abstract class AppModule {
         return new AppManager();
     }
 
-    @Binds
-    abstract Application.ActivityLifecycleCallbacks bindActivityLifecycle(ActivityLifecycle activityLifecycle);
-
-    @Binds
-    abstract FragmentManager.FragmentLifecycleCallbacks bindFragmentLifecycle(FragmentLifecycle fragmentLifecycle);
-
     @Singleton
     @Provides
     static List<FragmentManager.FragmentLifecycleCallbacks> provideFragmentLifecycles() {
         return new ArrayList<>();
     }
+
+    @Binds
+    abstract IRepositoryManager bindRepositoryManager(RepositoryManager repositoryManager);
+
+    @Binds
+    abstract Application.ActivityLifecycleCallbacks bindActivityLifecycle(
+            ActivityLifecycle activityLifecycle);
+
+    @Binds
+    abstract FragmentManager.FragmentLifecycleCallbacks bindFragmentLifecycle(
+            FragmentLifecycle fragmentLifecycle);
 
 
 }
--- pmvp/src/main/java/com/passin/pmvp/http/repository/RepositoryManager.java
+++ pmvp/src/main/java/com/passin/pmvp/http/repository/RepositoryManager.java
@@ -1,9 +1,15 @@
 package com.passin.pmvp.http.repository;
 
+import android.support.annotation.Nullable;
 import com.passin.pmvp.integration.cache.Cache;
 import com.passin.pmvp.integration.cache.CacheType;
 import com.passin.pmvp.util.Preconditions;
 import dagger.Lazy;
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import retrofit2.Retrofit;
@@ -17,6 +23,7 @@ import retrofit2.Retrofit;
  */
 @Singleton
 public class RepositoryManager implements IRepositoryManager {
+
     @Inject
     Lazy<Retrofit> mRetrofit;
     @Inject
@@ -30,22 +37,60 @@ public class RepositoryManager implements IRepositoryManager {
 
     /**
      * 根据传入的 Class 获取对应的 Retrofit service
-     *
+     * 
      * @param service
      * @param <T>
      * @return
      */
     @Override
     public synchronized <T> T obtainRetrofitService(Class<T> service) {
+        return createWrapperService(service);
+    }
+
+
+    private <T> T createWrapperService( Class<T> serviceClass) {
+        return (T) Proxy.newProxyInstance(serviceClass.getClassLoader(),
+                new Class<?>[]{serviceClass}, new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, @Nullable Object[] args)
+                            throws Throwable {
+                        if (method.getReturnType() == Observable.class) {
+                            // 如果方法返回值是 Observable 的话，则包一层再返回
+                            return Observable.defer(() -> {
+                                final T service = getRetrofitService(serviceClass);
+                                // 执行真正的 Retrofit 动态代理的方法
+                                return ((Observable) getRetrofitMethod(service, method)
+                                        .invoke(service, args))
+                                        .subscribeOn(Schedulers.io());
+                            })
+                                    .subscribeOn(Schedulers.single());
+                        }
+                        // 返回值不是 Observable 的话不处理
+                        final T service = getRetrofitService(serviceClass);
+                        return getRetrofitMethod(service, method).invoke(service, args);
+                    }
+                });
+    }
+
+
+    private <T> T getRetrofitService(Class<T> service) {
         if (mRetrofitServiceCache == null)
             mRetrofitServiceCache = mCachefactory.build(CacheType.RETROFIT_SERVICE_CACHE);
         Preconditions.checkNotNull(mRetrofitServiceCache, "Cannot return null from a Cache.Factory#build(int) method");
         T retrofitService = (T) mRetrofitServiceCache.get(service.getCanonicalName());
         if (retrofitService == null) {
-            retrofitService = mRetrofit.get().create(service);
-            mRetrofitServiceCache.put(service.getCanonicalName(), retrofitService);
+            synchronized (service) {
+                if (retrofitService == null) {
+                    retrofitService = mRetrofit.get().create(service);
+                    mRetrofitServiceCache.put(service.getCanonicalName(), retrofitService);
+                }
+            }
         }
         return retrofitService;
+    }
+
+    private <T> Method getRetrofitMethod(T service, Method method) throws NoSuchMethodException {
+        return service.getClass().getMethod(method.getName(), method.getParameterTypes());
     }
 
 }
